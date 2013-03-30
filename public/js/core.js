fe4c6d2e3563641a1c41e1fd48b9c7f7f5591197
Bobby Johnson <bobby.johnson@gmail.com>
2013-03-30 11:49:42 -0700

    adding and seting up bootstrap

--- public/js/core.js
+++ public/js/core.js
@@ -6,10 +6,11 @@
 require.config({
     baseUrl: '/js/',
     paths:{
-        jquery: '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min'
+        'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min',
+        'bootstrap': "libs/bootstrap.min"
     }
 });
 
-require(['jquery', 'mods/player'], function($, player){
+require(['jquery', 'mods/player', 'bootstrap'], function($, player){
     player.setup();
 });
