9dd66ee3d87622f6399de0c89ac16d3f28c82373
Bobby Johnson <bobby.johnson@gmail.com>
2013-03-30 11:12:20 -0700

    setting up a simple youtube api route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express = require('express'),
    routes = require('./routes'),
    user = require('./routes/user'),
+   youtube = require('./routes/youtube'),
    http = require('http'),
    path = require('path');
 
@@ -26,6 +27,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/videos', youtube.getVideos);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
