3622e6e45c00e78c21d8172e38269dfa35fbdc01
Bobby Johnson <bobby.johnson@gmail.com>
2013-03-30 15:35:54 -0700

    sends a video down to the users

--- listeners/player.js
+++ listeners/player.js
@@ -1,7 +1,29 @@
 'use strict';
 
+var region = 'JP',
+	youtube = require('youtube-feeds'),
+	_videos = [];
+
 exports.listen = function(io, socket){
+	socket.on('region', function(data){
+		region = data.region;
+	});
+
 	socket.on('play', function(data){
-			io.sockets.emit('play', data);
+		io.sockets.emit('play', data);
+	});
+
+
+	var getVideos = function(callback){
+		youtube.feeds.standard( region + "/most_popular", {time: 'today'}, function(err, videos){
+		  callback(videos.items);
 		});
+	};
+
+	getVideos(function(videos){
+		_videos = videos;
+		io.sockets.emit('play', videos[0]);
+	});
+
 };
+
